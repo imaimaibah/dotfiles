68fa885b851dbcbc9bebd9ee6d37f95154d9b386
Shin Imai <binaryninja0101@yahoo.com>
2024-07-31 11:33:05 +0100

    Updated

--- 5_setup_tools.sh
+++ 5_setup_tools.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
 BASEPATH=${0%%$(basename $0)}
-if [[ "x${BASEPATH}" == x"" ]];then
+if [[ "x${BASEPATH}" == x"" ]]; then
   BASEPATH=./
 fi
 
@@ -18,8 +18,8 @@ fi
 ## Gitmux
 LOCALBIN=~/.local/bin
 mkdir -p $LOCALBIN
-URL=$(curl -fLsq https://api.github.com/repos/arl/gitmux/releases/latest | 
-    awk '$1~/download_url/ && $2~/macOS_arm64/ {print $2}' | tr -d '"')
+URL=$(curl -fLsq https://api.github.com/repos/arl/gitmux/releases/latest |
+  awk '$1~/download_url/ && $2~/macOS_arm64/ {print $2}' | tr -d '"')
 curl -Lsqf $URL | tar zx -C $LOCALBIN/ && chmod +x $LOCALBIN/gitmux || echo "Downloadin gitmux failed"
 
 ## kube-tmux
@@ -35,8 +35,8 @@ curl -Lsqf $URL | tar zx -C $LOCALBIN/ && chmod +x $LOCALBIN/gitmux || echo "Dow
   curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_moon.tmTheme
   curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_storm.tmTheme
   bat cache --build
-  if ! grep -- "--theme=";then
-    echo '--theme="tokyonight_night"' >> "$(bat --config-dir)/config"
+  if ! grep -- "--theme="; then
+    echo '--theme="tokyonight_night"' >>"$(bat --config-dir)/config"
   fi
 )
 
@@ -46,28 +46,29 @@ helm plugin install https://github.com/jkroepke/helm-secrets
 
 # Install Krew and plugins
 (
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
+  set -x
+  cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
   ~/.krew/bin/kubectl-krew install ctx ns stern
 )
 
-### Create symlinks for dotfiles 
-for file in $(ls ${BASEPATH}*_custom);do
+### Create symlinks for dotfiles
+for file in $(ls ${BASEPATH}*_custom); do
   dotfile=${file##*/}
-  ln -fs $(pwd)/${dotfile} ~/.${dotfile} 
+  ln -fs $(pwd)/${dotfile} ~/.${dotfile}
 done
 
-if ! grep -E "source-file.*\.tmux\.conf_custom" ~/.tmux.conf.local &>/dev/null;then
-  echo "source-file ~/.tmux.conf_custom" >> ~/.tmux.conf.local
-  tmux 
+if ! grep -E "source-file.*\.tmux\.conf_custom" ~/.tmux.conf.local &>/dev/null; then
+  echo "source-file ~/.tmux.conf_custom" >>~/.tmux.conf.local
+  tmux
 fi
 
-if ! grep -E "source.*\.zshrc_custom" ~/.zshrc;then
-  echo "source ~/.zshrc_custom" >> ~/.zshrc
+if ! grep -E "source.*\.zshrc_custom" ~/.zshrc; then
+  echo "source ~/.zshrc_custom" >>~/.zshrc
   . ~/.zshrc
 fi
