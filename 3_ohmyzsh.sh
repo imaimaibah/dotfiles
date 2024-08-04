30ece87ca4bdec09cdd9734db514ef9e44fb2b44
Shin Imai <binaryninja0101@yahoo.com>
2024-08-04 21:35:46 +0100

    Fix

--- 3_ohmyzsh.sh
+++ 3_ohmyzsh.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir -p ~/.config/zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh |sh -s
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
