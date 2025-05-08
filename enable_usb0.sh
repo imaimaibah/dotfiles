9c492401f4e0282597daa57c2c29c1c7058b79a8
Shin Imai <binaryninja0101@yahoo.com>
2025-05-08 19:04:14 +0100

    enable usb0

---
+++ enable_usb0.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+
+sudo -i
+
+if ! grep "dwc2,g_ether" /boot/firmware/cmdline.txt; then
+  sed -i 's/rootwait /&modules-load=dwc2,g_ether ' /boot/firmware/cmdline.txt
+fi
+
+if ! grep "dtoverlay=dwc2" /boot/firmware/config.txt; then
+  sed -i 's/\[all\]/&\ndtoverlay=dwc2' /boot/firmware/config.txt
+fi
+
+if [ ! -e "/etc/NetworkManager/system-connections/ethernet-usb0.nmconnection" ]; then
+  nmcli con add type ethernet con-name ethernet-usb0
+  sed -i 's/type=ethernet/&\nautoconnect=true\ninterface-name=usb0\n/' /etc/NetworkManager/system-connections/ethernet-usb0.nmconnection
+  sed -i '/ipv4/,/^$/s#method=.*#method=shared#' /etc/NetworkManager/system-connections/ethernet-usb0.nmconnection
+fi
+
+cat <<EOF >/usr/local/sbin/usb-gadget.sh
+#!/bin/bash
+
+nmcli con up ethernet-usb0
+EOF
+
+chmod a+rx /usr/local/sbin/usb-gadget.sh
+
+cat <<EOF >/lib/systemd/system/usbgadget.service
+[Unit]
+Description=My USB gadget
+After=NetworkManager.service
+Wants=NetworkManager.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/local/sbin/usb-gadget.sh
+
+[Install]
+WantedBy=sysinit.target
+
+systemctl enable usbgadget.service
+
+read -r -p "Reboot now. Press Enter: "
+reboot
